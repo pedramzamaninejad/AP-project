6b5ab399dfd5c273dbb967e98d9a9a9fe4f283f5
pedram.zamaninezhad <pzamaninejad@yahoo.com>
2024-06-06 16:14:39 +0330

    using composition relation between library and book clases, add info and update method to book

--- p1.py
+++ p1.py
@@ -1,27 +1,31 @@
 class UniLibManager:
     ...
 
+
 class Library:
-    def __init__(self, path: str):
+    def __init__(self, path: str) -> None:
         self.path = path
         self.name = self.path.split('/')[-1]
         self.books: list[dict] = []
 
-    def read_book_from_csv(self):
+    def read_book_from_csv(self) -> None:
         import pandas as pd
         import os
 
         os.chdir(f'{self.path}')
         for i in os.listdir():
-            book = pd.read_csv(self.path+"/"+i, header=0, delimiter=' ')
+            book = pd.read_csv(self.path + "/" + i, header=0, delimiter=' ')
 
         self.books += [
-                        {'name': book.loc[i]['name'],
-                         'writers': book.loc[i]['writers'],
-                         'year_published': book.loc[i]['year_published'],
-                         'keyword': book.loc[i]['keyword'],
-                         } for i in range(len(book))
+            Book(book.loc[i]['name'],
+                 book.loc[i]['year_published'],
+                 book.loc[i]['writers'],
+                 book.loc[i]['keyword']) for i in range(len(book))
         ]
+
+    def __len__(self) -> int:
+        return len(self.books)
+
 
 class Book:
     def __init__(self, name: str, publish_year: str, writers: str | list[str], key_word: str):
@@ -29,3 +33,19 @@ class Book:
         self.published_year = publish_year
         self.writers = writers
         self.key_word = key_word
+
+    def info(self):
+        return {'name': self.name,
+                'year_published': self.published_year,
+                'writers': self.writers,
+                'keyword': self.key_word}
+
+    def update(self, name=None, published_year=None, writers=None, keyword=None):
+        if name is not None:
+            self.name = name
+        if published_year is not None:
+            self.published_year = published_year
+        if writers is not None:
+            self.writers = writers
+        if keyword is not None:
+            self.key_word = keyword
